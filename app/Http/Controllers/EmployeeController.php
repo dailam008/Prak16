fbfd18328c7d1566840b4d2b1c579ac0bd2c4393
dailam008 <daimoh442@gmail.com>
2024-12-19 05:10:32 +0700

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use App\Models\Employee;
+use App\Models\Position;
+
+
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pageTitle = 'Employee List';
+        // ELOQUENT
+        $employees = Employee::all();
+        return view('employee.index', [
+            'pageTitle' => $pageTitle,
+            'employees' => $employees
+        ]);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $pageTitle = 'Create Employee';
+        // ELOQUENT
+        $positions = Position::all();
+        return view('employee.create', compact('pageTitle', 'positions'));
+    }
+    public function store(Request $request)
+    {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Get File
+        $file = $request->file('cv');
+
+        if ($file != null) {
+            $originalFilename = $file->getClientOriginalName();
+            $encryptedFilename = $file->hashName();
+
+            // Store File
+            $file->store('public/files');
+        }
+
+        // ELOQUENT
+        $employee = New Employee;
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+
+        if ($file != null) {
+            $employee->original_filename = $originalFilename;
+            $employee->encrypted_filename = $encryptedFilename;
+        }
+
+        $employee->save();
+
+        return redirect()->route('employees.index');
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $pageTitle = 'Employee Detail';
+        // ELOQUENT
+        $employee = Employee::find($id);
+        return view('employee.show', compact('pageTitle', 'employee'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $pageTitle = 'Edit Employee';
+        // ELOQUENT
+        $positions = Position::all();
+        $employee = Employee::find($id);
+        return view('employee.edit', compact(
+            'pageTitle',
+            'positions',
+            'employee'
+        ));
+    }
+    public function update(Request $request, string $id)
+    {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+        // ELOQUENT
+        $employee = Employee::find($id);
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+        return redirect()->route('employees.index');
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // Temukan employee berdasarkan ID
+        $employee = Employee::findOrFail($id);
+
+        // Hapus file CV jika ada
+        if ($employee->encrypted_filename) {
+            $filePath = 'public/files/' . $employee->encrypted_filename;
+            if (Storage::exists($filePath)) {
+                Storage::delete($filePath);
+            }
+        }
+
+        // Hapus data employee dari database
+        $employee->delete();
+
+        return redirect()->route('employees.index');
+
+    }
+
+    public function downloadFile($employeeId)
+    {
+        $employee = Employee::find($employeeId);
+        $encryptedFilename = 'public/files/' . $employee->encrypted_filename;
+        $downloadFilename = Str::lower($employee->firstname . '_' . $employee->lastname . '_cv.pdf');
+        if (Storage::exists($encryptedFilename)) {
+            return Storage::download($encryptedFilename, $downloadFilename);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,120 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Ini adalah file rute untuk aplikasi Anda. Anda dapat menambahkan
+| rute di sini dan menyesuaikan middleware atau kontroler sesuai kebutuhan.
+|
+*/
+
+// Rute default: Redirect ke halaman login
+Route::get('/', function () {
+    return redirect('/login'); // Arahkan langsung ke halaman login
+});
+
+// Authentication routes (tanpa fitur register)
+Auth::routes(['register' => false]);
+
+// Rute yang membutuhkan autentikasi
+Route::middleware('auth')->group(function () {
+
+    // Halaman Home
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // Halaman Profile
+    Route::get('/profile', [ProfileController::class, '__invoke'])->name('profile');
+
+    // Rute CRUD untuk karyawan (EmployeeController)
+    Route::resource('employees', EmployeeController::class);
+});
+
+Route::get('/local-disk', function () {
+    Storage::disk('local')->put('local-example.txt', 'This is local example content');
+    return asset('storage/local-example.txt');
+});
+
+Route::get('/public-disk', function () {
+    Storage::disk('public')->put('public-example.txt', 'This is public example content');
+    return asset('storage/public-example.txt');
+});
+
+
+Route::get('/retrieve-local-file', function () {
+    if (Storage::disk('local')->exists('local-example.txt')) {
+        $contents = Storage::disk('local')->get('local-example.txt');
+    } else {
+        $contents = 'File does not exist';
+    }
+
+    return $contents;
+});
+
+Route::get('/retrieve-public-file', function () {
+    if (Storage::disk('public')->exists('public-example.txt')) {
+        $contents = Storage::disk('public')->get('public-example.txt');
+    } else {
+        $contents = 'File does not exist';
+    }
+
+    return $contents;
+});
+
+
+Route::get('/download-local-file', function () {
+    return Storage::download('local-example.txt', 'local file');
+});
+
+Route::get('/download-public-file', function () {
+    return Storage::download('public/public-example.txt', 'public file');
+});
+
+
+Route::get('/file-url', function () {
+    // Just prepend "/storage" to the given path and return a relative URL
+    $url = Storage::url('local-example.txt');
+    return $url;
+});
+
+Route::get('/file-size', function () {
+    $size = Storage::size('local-example.txt');
+    return $size;
+});
+
+Route::get('/file-path', function () {
+    $path = Storage::path('local-example.txt');
+    return $path;
+});
+
+Route::get('/upload-example', function () {
+    return view('upload_example');
+});
+
+Route::post('/upload-example', function (Request $request) {
+    $path = $request->file('avatar')->store('public');
+    return $path;
+})->name('upload-example');
+
+Route::get('/delete-local-file', function (Request $request) {
+    Storage::disk('local')->delete('local-example.txt');
+    return 'Deleted';
+});
+Route::get('/delete-public-file', function (Request $request) {
+    Storage::disk('public')->delete('public-example.txt');
+    return 'Deleted';
+});
+
+Route::get('download-file/{employeeId}', [
+    EmployeeController::class,
+    'downloadFile'
+])->name('employees.downloadFile');
+
+Route::get('/employees/{id}/download', [EmployeeController::class, 'downloadFile'])->name('employees.download');
